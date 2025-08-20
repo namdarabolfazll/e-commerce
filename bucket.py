72853e2496ac5008c308a88dde865e2c2b37593f
abolfazl <namdrpyx@gmail.com>
2025-08-20 11:14:16 +0330

    celery and botoe3 connection

---
+++ bucket.py
@@ -0,0 +1,16 @@
+import boto3
+from django.conf import settings
+
+class Bucket(object):
+    '''CDN bucket manager
+
+    init method create connection
+    '''
+    def __init__(self):
+        session = boto3.session.Session()
+        self.conn = session.client(
+            service_name=settings.AWS_SERVICE_NAME,
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+        )
